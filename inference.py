ac022c1014f3219c1fb9912b9c158aed722b61c1
Анатолий Филиппов <a.filippov@yclients.tech>
2025-05-12 00:24:12 +0300

    (chore) analyze func in inference

--- inference.py
+++ inference.py
@@ -6,11 +6,11 @@ from SkinTypeChecker import SkinTypeChecker
 import matplotlib.pyplot as plt
 import numpy as np
 
-model_path = "/home/alexx/deep_learning/data/skin_types/mobilenetv2.pth"
+model_path = "mobilenetv2.pth"
 
-image_path_acne = "/home/alexx/deep_learning/data/uniq/skin_types/test1/acne/19_jpg.rf.231a085ac891ea2bed663ee140b9aa21.jpg"
-image_path_dry = "/home/alexx/deep_learning/data/uniq/skin_types/test1/dry/dry16_jpg.rf.ee2726f8440e10c24acba66999904b66.jpg"
-image_path_oil = "/home/alexx/deep_learning/data/uniq/skin_types/test1/oil/images28_jpeg.rf.23fb36cda5a74bb378d89feb6925c5ac.jpg"
+image_path_acne = "skin_types/test1/acne/19_jpg.rf.231a085ac891ea2bed663ee140b9aa21.jpg"
+image_path_dry = "skin_types/test1/dry/dry16_jpg.rf.ee2726f8440e10c24acba66999904b66.jpg"
+image_path_oil = "skin_types/test1/oil/images28_jpeg.rf.23fb36cda5a74bb378d89feb6925c5ac.jpg"
 
 sc = SkinTypeChecker(model_path)
 
@@ -29,8 +29,17 @@ def imshow(inp, title=None, plt_ax=plt, default=False):
     plt_ax.grid(False)
 
 
-for path in ( image_path_acne, image_path_dry,  image_path_oil):
-    label, dlass_id, probs = sc.analyze(path)
+# for path in (image_path_acne, image_path_dry,  image_path_oil):
+#     label, dlass_id, probs = sc.analyze(path)
+#     print(label, probs)
+#     # imshow(sc.image())
+#     # plt.show()
+
+# вызов анализа конкретного файла
+def analyze(image_path: str) -> str:
+    label, dlass_id, probs = sc.analyze(image_path)
     print(label, probs)
-    imshow(sc.image())
-    plt.show()
+    return label
+
+#пример вызова
+analyze("temp/01.jpg")
